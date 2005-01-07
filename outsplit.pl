7294396826707d0f5044d34dc332d604ca3d6a38
Reed A. Cartwright <reed@cartwrig.ht>
2005-01-07 11:33:51 +0000

    git-svn-id: svn://scit.us/dawg/trunk@167 7e5b8c5c-87e2-0310-a459-8d91125b37d5

---
+++ outsplit.pl
@@ -0,0 +1,48 @@
+#! /usr/bin/perl -w
+# Copyright (2005) Reed A. Cartwright.  All rights reserved.
+#
+# outsplit.pl is used to extract sequences from Fasta and Phylip files
+# 
+# usage perl -w outsplit.pl <file> <id>
+#
+# if <id> is "all" it it creates a directory and
+# creates a new file for each alignment
+
+use strict;
+use File::Basename;
+use File::Path;
+use File::Spec::Functions;
+
+my ($file, $id) = @ARGV;
+
+local $/;
+
+open( FILE, $file) or die("Error opening $file.");
+
+my $text = <FILE>;
+
+close(FILE);
+
+my @blocks = split(/\[DataSet \d+\].*\n/, $text);
+
+if($id ne 'all')
+{
+	print $blocks[$id];	
+}
+else
+{
+	my ($name,$dir,$ext) = fileparse($file, qr{\..*});
+	my $outdir = catdir($dir, $name);
+	print "Creating directory $outdir\n";
+	mkpath($outdir) unless (-d $outdir);
+	chdir($outdir) or die("Unable to change directory.");
+	
+	foreach my $i (1..$#blocks)
+	{
+		my $out = "${name}_$i$ext";
+		print "Creating file $out\n";
+		open(OUT, ">$out") or die("Unable to open file.");
+		print OUT $blocks[$i];
+		close(OUT);
+	}
+}
