330b3e010fd110120e183ecf4ef2a4f91683913c
Reed A. Cartwright <reed@cartwrig.ht>
2006-06-06 07:13:36 +0000

    git-svn-id: svn://scit.us/dawg/current@290 7e5b8c5c-87e2-0310-a459-8d91125b37d5

---
+++ vs/test/test.cpp
@@ -0,0 +1,19 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "rand.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	zipf_distribution<> zipf_dist(1.5);
+	boost::variate_generator<DawgRng&, zipf_distribution<> > rand_zipf(g_rng, zipf_dist);
+	for(int i=0;i<300;++i)
+		cout << rand_zipf() << " ";
+	cout << endl;
+	return 0;
+}
+
