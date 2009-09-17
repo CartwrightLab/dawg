1d6994a4b9d9b9000d4dbb0a857df7a684b40110
Reed A. Cartwright <reed@cartwrig.ht>
2009-09-17 00:45:28 +0000

    removal of experimetnal Sequence2 class, we will use a finger tree instead
    git-svn-id: svn://scit.us/dawg/branches/newseq@386 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/nseq.cpp
+++ src/nseq.cpp
@@ -6,20 +6,7 @@
 
 #include "nseq.h"
 
-ResidueFactory makeSeq;
-
 using namespace std;
-
-void printseq(const Sequence2 &seq2) {
-	cout << seq2.size() << ": ";
-	
-	for(Sequence2::const_iterator nit = seq2.begin(); nit != seq2.end(); ++nit)
-		cout << (int)((nit)->base) << "-" << ((nit)->rate) << "    ";
-	cout << endl;
-	for(Sequence2::Parts::const_iterator it = seq2._parts().begin(); it != seq2._parts().end(); ++it)
-		cout << it->size() << " ";
-	cout << endl;
-}
 
 template<class _T, class _W>
 void printnode(typename dawg::finger_tree<_T,_W>::node::pointer p) {
@@ -36,32 +23,34 @@ long unsigned int ul_find[256];
 double d_find[256];
 dawg::evo_node_weight<> w_find[256];
 typedef dawg::finger_tree<dawg::residue, dawg::evo_node_weight<> > FT;
-//typedef dawg::finger_tree<dawg::evo_node, weight> FT;
 extern FT::iterator tit;
 FT::iterator tit;
 
 int main(int argc, char* argv[]) {
 	FT tree;
 	char in;
+	dawg::residue_factory make_seq;
 	
-	for(int i=0;i<50;++i) {
-		tree.insert(tree.end(), dawg::residue());
+	std::string ss = "acgtacgtacgtACGTACGTaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	
+	make_seq(ss.begin(),ss.end(),tree);
+	
+	for(int i=0;i<ss.length();++i) {
 		ul_find[i] = i;
 		d_find[i] = i;
 		w_find[i] = dawg::evo_node_weight<>(i,i);
 	}
 		
-	for(int i=0;i<50;++i) {
-		tit = tree.find(ul_find[i]);
-		cout << tit->val.base() << " ";
-		tit = tree.find(d_find[i]);
-		cout << tit->val.base() << " ";
-		tit = tree.find(w_find[i]);
-		cout << tit->val.base() << endl;
+	for(int i=0;i<ss.length();++i) {;
+		cout << make_seq.decode(tree[ul_find[i]].base());
+//		cout << tit->val.base() << " ";
+//		tit = tree.find(d_find[i]);
+//		cout << tit->val.base() << " ";
+//		tit = tree.find(w_find[i]);
+//		cout << tit->val.base() << endl;
 	}
+	cout << endl;
 		
 	return 0;
 }
-
-ResidueFactory Sequence2::makeSeq;
 
--- src/nseq.h
+++ src/nseq.h
@@ -198,6 +198,8 @@ public:
 	typedef _T data_type;
 	typedef _W weight_type;
 	typedef detail::finger_tree_node_iterator<_T, _W> iterator;
+	typedef node& reference;
+	typedef const node& const_reference;
 
 	finger_tree() : head() {
 		head.up = &head;
@@ -239,7 +241,10 @@ public:
 			if(right != NULL)
 				delete right;
 		}
-
+		
+		operator data_type() { return val; }
+		operator data_type() const { return val; }
+		
 		pointer rotate_left() {
 			pointer x = right;
 			right = x->left;
@@ -346,6 +351,15 @@ public:
 		return iterator(x);
 	}
 	
+	void push_back(const data_type &val) {
+		insert(end(),val);
+	}
+	
+	template<class _P>
+	data_type operator[](const _P &pos) {
+		return find<_P>(pos)->val;
+	}
+	
 	template<class _P>
 	iterator find(const _P &pos) {
 		typename node::pointer p = head.up;
@@ -378,60 +392,30 @@ private:
 	node head;
 };
 
-};
-
-class ResidueFactory
-{
+class residue_factory {
 public:
 	typedef unsigned int model_type;
-	static const model_type modelDNA = 0;
-	static const model_type modelRNA = 1;
-	static const model_type modelAA  = 2;
-	static const model_type modelCod = 3;
-
-	struct Residue {
-		typedef unsigned char base_type;
-		typedef float rate_type;
-		
-		base_type base;
-		rate_type rate;
-		
-		Residue() { }
-		
-		Residue(base_type b, rate_type r)
-			: base(b), rate(r) { }
-	};
-
-	typedef std::vector<Residue> Block; 
-	typedef std::list<Block> DataStore;
-	typedef Block::iterator seq_iterator;
-	typedef Block::const_iterator const_seq_iterator;
+	typedef residue_factory self_type;
 	
-	template<typename It>
-	seq_iterator operator()(It b, It e) {
-		// should speed test
-		store.push_back(Block());
-		store.back().reserve(e-b);
-		std::transform(b, e, std::back_inserter(store.back()),
-			std::bind1st(std::mem_fun(&ResidueFactory::makeResidue), this));
-		return store.back().begin();
-	}
-	seq_iterator operator()(seq_iterator b, seq_iterator e) {
-		store.push_back(Block(b,e));
-		return store.back().begin();
-	}
-	seq_iterator operator()(const_seq_iterator b, const_seq_iterator e) {
-		store.push_back(Block(b,e));
-		return store.back().begin();
+	static const model_type DNA = 0;
+	static const model_type RNA = 1;
+	static const model_type AA  = 2;
+	static const model_type CODON = 3;
+	
+	template<class It, class D>
+	void operator()(It b, It e, D &dest) {
+		std::transform(b, e, std::back_inserter(dest),
+			std::bind1st(std::mem_fun(&self_type::make_residue), this));
 	}
 
-	struct biop : public std::binary_function<char, double, Residue> {
-		biop(const ResidueFactory *o) : obj(o) { }
-		const ResidueFactory *obj;
-		Residue operator()(char ch, double d) {
-			return obj->makeResidue(ch,d);
+	struct biop : public std::binary_function<char, double, residue> {
+		biop(const residue_factory *o) : obj(o) { }
+		const residue_factory *obj;
+		residue operator()(char ch, double d) {
+			return obj->make_residue(ch,d);
 		}
 	};
+	/*
 	template<typename It1, typename It2>
 	seq_iterator operator()(It1 b, It1 e, It2 r) {
 		store.push_back(Block());
@@ -439,23 +423,24 @@ public:
 		std::transform(b, e, r, std::back_inserter(store.back()), biop(this));
 		return store.back().begin();
 	}
+	*/
 
-	inline void SetModel(model_type a) { seq_model = a; };
+	inline void model(model_type a) {_model = a; };
 	
-	Residue makeResidue(char ch) const {
-		return Residue(encode(ch), 1.0);
+	residue make_residue(char ch) const {
+		return residue(encode(ch), 1.0, 0, 1.0);
 	}
-	Residue makeResidue(char ch, double d) const {
-		return Residue(encode(ch), d);
+	residue make_residue(char ch, double d) const {
+		return residue(encode(ch), d, 0, 1.0);
 	}
 	
-	Residue::base_type encode(char ch) const {
-		static Residue::base_type dna[] = {0,1,3,2};
-		if(seq_model == modelDNA || seq_model == modelRNA)
+	residue::base_type encode(char ch) const {
+		static residue::base_type dna[] = {0,1,3,2};
+		if(_model == DNA || _model == RNA)
 			return dna[(ch&6u) >> 1];
 		return ~0;
 	}
-	char decode(Residue::base_type r) const {
+	char decode(residue::base_type r) const {
 		static char dna[] = "ACGT";
 		static char rna[] = "ACGU";
 		// Include O & U, two rare amino acids
@@ -465,14 +450,14 @@ public:
 		// These can later be converted to three nucleotides or 1 amino acid
 		static char cod[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.";
 
-		switch(seq_model) {
-		case modelDNA:
+		switch(_model) {
+		case DNA:
 			return dna[r];
-		case modelRNA:
+		case RNA:
 			return rna[r];
-		case modelAA:
+		case AA:
 			return aa[r];
-		case modelCod:
+		case CODON:
 			return cod[r];
 		default:
 			break;
@@ -480,314 +465,13 @@ public:
 		return '?';
 	}
 	
-	void clear() { store.clear(); }
+	residue_factory() : _model(DNA) { }
 
 protected:
-	DataStore store;
-	model_type seq_model;
+	model_type _model;
 };
 
-template<typename ith, typename itl>
-class residue_iterator {
-public:
-	typedef ith high_iterator;
-	typedef itl low_iterator;
-	typedef typename low_iterator::reference reference;
-	typedef typename low_iterator::pointer pointer;
-	typedef typename low_iterator::value_type value_type;
-	typedef std::bidirectional_iterator_tag iterator_category;
-
-	typedef residue_iterator<high_iterator, low_iterator> this_type;
-	
-	residue_iterator() {}
-	residue_iterator(high_iterator h, high_iterator e)
-		: hit(h), hit_end(e) {
-		while(dirtyhit())
-			++hit;
-		lit = (hit != hit_end) ? hit->begin() : low_iterator();
-	}
-	residue_iterator(high_iterator h, high_iterator(e), low_iterator l)
-		: hit(h), hit_end(e), lit(l) {
-		/* assumes consistency */
-	}
-
-	inline bool dirtyhit() {
-		return ( hit != hit_end && (hit->empty() || hit->IsDeletion() ));
-	}
-
-	this_type& operator++() {
-		if(++lit == hit->end()) {
-			do { ++hit; } while(dirtyhit());
-			lit = (hit != hit_end) ? hit->begin() : low_iterator();
-		}
-		return *this;
-	}
-	this_type operator++(int) {
-		this_type ret = *this;
-		++(*this);
-		return ret;
-	}
-	this_type& operator--() {
-		if(lit == hit->begin()) {
-			do {--hit; } while(dirtyhit());
-			lit = (hit != hit_end) ? --hit->end() : low_iterator();
-		}
-		return *this;
-	}
-	this_type operator--(int) {
-		this_type ret = *this;
-		--(*this);
-		return ret;
-	}
-	bool operator==(const this_type &other) const {
-		return (hit==other.hit && (hit == hit_end || lit == other.lit));
-	}
-	bool operator!=(const this_type &other) const {
-		return !(*this == other);
-	}
-
-	reference operator*() const {
-		return *lit;
-	}
-	pointer operator->() const {
-		return (&**this);
-	}
-
-	high_iterator _hit() { return hit; }
-	low_iterator _lit() { return lit; }
-	
-protected:
-	high_iterator hit;
-	high_iterator hit_end;
-	low_iterator lit;
-};
-
-class Sequence2 {
-public:
-	// A sequence contains two important pieces of data
-	// 1. a vector of vectors of nucleotides.  This contains the raw Part data
-	// 2. a vector of Part information
-	
-	static ResidueFactory makeSeq;
-		
-	class Part {
-	public:
-		typedef unsigned int color_type;
-		typedef ResidueFactory::Block data_type;
-		typedef data_type::value_type value_type;
-		typedef data_type::iterator iterator;
-		typedef data_type::const_iterator const_iterator;
-		typedef data_type::reference reference;
-		typedef data_type::const_reference const_reference;
-		typedef data_type::size_type size_type;
-		
-		static const color_type BranchInc   =  0x2;
-		static const color_type MaskBranch	= ~0x1;
-		static const color_type MaskType	=  0x1; // 1
-		static const color_type TypeDel		=  0x1; // 1
-		static const color_type TypeExt		=  0x0; // 0
-		
-		Part(iterator it, size_type len, color_type uT, color_type uB) :
-			itBegin(it), szLength(len), color((uT & MaskType) | (uB & MaskBranch))
-			{ }
-		Part(iterator it, size_type len, color_type u) :
-			itBegin(it), szLength(len), color(u)
-			{ }
-		
-		inline color_type GetBranch() const { return color & MaskBranch; }
-		inline color_type GetType()   const { return color & MaskType; }
-		inline color_type GetColor()  const { return color; }
-		
-		inline void SetType(color_type u)   { color =  (u & MaskType)   | (color & ~MaskType); }
-		inline void SetBranch(color_type u) { color =  (u & MaskBranch) | (color & ~MaskBranch); }
-		
-		inline void SetColor(color_type uT, color_type uB)
-			{ color =  (uT & MaskType) | (uB & MaskBranch); }
-		inline void SetColor(color_type u) { color = u; }
-		inline bool IsType(color_type u) const { return (GetType() == (u & MaskType)); }
-		inline bool IsBranch(color_type u) const { return (GetBranch() == (u & MaskBranch)); }
-		inline bool IsDeletion() const { return IsType(TypeDel); }
-		inline bool IsExtant()   const { return IsType(TypeExt); }
-		
-		inline bool empty() const { return (szLength == 0); }
-
-		inline iterator begin() { return itBegin;}
-		inline iterator end() { return itBegin+szLength; }
-		inline iterator mid(size_type pos) {
-			//assert pos < length ?
-			return itBegin+pos;
-		}
-
-		inline const_iterator begin() const { return itBegin; }
-		inline const_iterator end() const { return itBegin+szLength; }
-		inline const_iterator mid(size_type pos) const {
-			//assert pos < length ?
-			return itBegin+pos;
-		}
-		
-		inline size_type size() const { return szLength; }
-		
-		inline Part split(iterator loc) {
-			Part ret;
-			if(loc == itBegin)
-				return ret;
-			ret.itBegin = loc;
-			ret.szLength = itBegin+szLength-loc;
-			ret.color = color;
-			szLength -= ret.szLength;
-			return ret;			
-		}
-		
-		inline Part clone() const {
-			return Part(makeSeq(begin(), end()), szLength, color);
-		}
-			
-	protected:
-		iterator itBegin;
-		size_type szLength;
-		color_type color;
-
-	private:
-		Part() : szLength(0) { };
-		//Part(const Part &p);
-		//Part & operator = (const Part &p);
-		
-		friend class Sequence2;
-	};
-
-	typedef std::list<Part> Parts;
-	typedef residue_iterator<Parts::iterator, Part::iterator> iterator;
-	typedef residue_iterator<Parts::const_iterator, Part::const_iterator> const_iterator;
-	typedef Part::size_type size_type;
-
-	Sequence2() : szLength(0) {
-	}
-
-	template<typename It1, typename It2>
-	void append(It1 b, It1 e, It2 r) {
-		ResidueFactory::seq_iterator it = makeSeq(b,e,r);
-		size_type len = e-b;
-		parts.push_back(Part(it, len, Part::TypeExt, 0));
-		szLength += len;
-	}
-
-	template<typename It1>
-	void append(It1 b, It1 e) {
-		ResidueFactory::seq_iterator it = makeSeq(b,e);
-		size_type len = e-b;
-		parts.push_back(Part(it, len, Part::TypeExt, 0));
-		szLength += len;
-	}
-	
-	void append(const Sequence2 &s2) {
-		parts.insert(parts.end(), s2.parts.begin(), s2.parts.end());
-		szLength += s2.size();
-	}
-	
-	void insert(size_type pos, const Sequence2 &s2) {
-		iterator it = mid(pos);
-		Parts::iterator hit = it._hit();
-		if(hit == parts.end()) {
-			parts.insert(hit, s2.parts.begin(), s2.parts.end());
-		} else {
-			Part tail = hit->split(it._lit());
-			parts.insert(++hit, s2.parts.begin(), s2.parts.end());
-			if(!tail.empty())
-				parts.insert(hit, tail);
-		}
-		szLength += s2.size();
-	}
-	
-	void erase(size_type pos, size_type len) {
-		iterator it = mid(pos);
-		Parts::iterator hit = it._hit();
-		if(hit == parts.end())
-			return; //nothing to do
-		// find start location and split if needed
-		Part tail = hit->split(it._lit());
-		if(!tail.empty()) {
-			parts.insert(++hit, tail);
-			--hit;
-		}
-		// hit now contains the first part of the deletion
-		// find the last part
-		while(hit != parts.end() && hit->size() <= len) {
-			// mark all of this part as deleted
-			hit->SetType(Part::TypeDel);
-			len -= hit->size();
-			// advance past deletions
-			do { ++hit; } while(hit->IsDeletion());
-		}
-		if(hit == parts.end() || len == 0)
-			return; // done
-		// split again
-		Part tail2 = hit->split(hit->begin()+len);
-		hit->SetType(Part::TypeDel);
-		parts.insert(++hit, tail2);
-	}
-	
-	void optimize() {
-		if(parts.empty())
-			return;
-		Parts::iterator itpp = parts.begin();
-		Parts::iterator it = itpp++;
-		while(itpp != parts.end()) {
-			if(it->end() == itpp->begin() && it->GetColor() == itpp->GetColor()) {
-				it->szLength += itpp->szLength;
-				itpp = parts.erase(itpp);
-			} else {
-				++it;
-				++itpp;
-			}
-		}
-	}
-	
-	Sequence2 clone() {
-		Sequence2 ret;
-		ret.szLength = szLength;
-		std::transform(parts.begin(), parts.end(), std::back_inserter(ret.parts),
-			std::mem_fun_ref(&Part::clone));
-		return ret;
-	}
-
-	size_type size() const { return szLength; }
-
-	// begin, end, and mid cycle through extant residues
-
-	inline iterator begin() {
-		return iterator(parts.begin(),parts.end());
-	}
-	inline iterator end() {
-		return iterator(parts.end(),parts.end());
-	}
-	inline const_iterator begin() const {
-		return const_iterator(parts.begin(), parts.end());
-	}
-	inline const_iterator end() const {
-		return const_iterator(parts.end(),  parts.end());
-	}
-		
-	inline iterator mid(size_type pos) {
-		if(pos >= size())
-			return end();
-		Parts::iterator it = parts.begin();
-		for(; it != parts.end(); ++it) {
-			if(it->IsDeletion())
-				continue;
-			if(pos < it->size())
-				break;
-			 pos -= it->size();
-		}
-		return (it == parts.end()) ? iterator(parts.end(), parts.end())
-			: iterator(it, parts.end(), it->begin()+pos);
-	}
-	
-	const Parts& _parts() const { return parts; }
-
-protected:
-	Parts parts;
-	size_type szLength;
-};
+}; // namespace dawg
 
 
 #endif
