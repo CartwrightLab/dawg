cd990393268fbe3253982ae709c1667dd09da73f
Reed A. Cartwright <reed@cartwrig.ht>
2009-12-03 02:12:04 +0000

    functional parser
    git-svn-id: svn://scit.us/dawg/current@453 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/wood.h
+++ src/include/dawg/wood.h
@@ -7,8 +7,40 @@
  ****************************************************************************/
 
 #include <boost/scoped_ptr.hpp>
+#include <boost/spirit/include/qi.hpp>
 
 namespace dawg {
+
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+template <typename Iterator>
+struct newick_grammar : qi::grammar<Iterator, ascii::space_type> {
+	//http://evolution.genetics.washington.edu/phylip/newick_doc.html
+
+	newick_grammar() : newick_grammar::base_type(start) {
+		using boost::spirit::double_;
+		using boost::spirit::lexeme;
+		using boost::spirit::char_;
+		using ascii::space;
+		
+		start = node >> ';';
+		node =  tip | inode;
+		tip = label >> -(':' >> double_);
+		inode = '(' >> (node % ',') >> ')' >> -(label || (':' >> double_));
+		label = unquoted | quoted;
+		unquoted = lexeme[+(char_ - (char_("()[]':;,")|space))];
+		quoted = lexeme['\'' >> (*(char_ - '\'') >> *(char_("\'") >> char_("\'") >> *(char_ - '\''))) >> '\''];
+	}
+	
+	qi::rule<Iterator, ascii::space_type> start;
+	qi::rule<Iterator, ascii::space_type> node;
+	qi::rule<Iterator, ascii::space_type> tip;
+	qi::rule<Iterator, ascii::space_type> inode;
+	qi::rule<Iterator, ascii::space_type> label;
+	qi::rule<Iterator, ascii::space_type> unquoted;
+	qi::rule<Iterator, ascii::space_type> quoted;
+};
 
 // Represents a phylogenetic tree structure
 class wood {
@@ -21,6 +53,18 @@ public:
 	};
 	
 	boost::scoped_ptr<node> root;
+	
+	template<typename Iterator>
+	bool parse(Iterator first, Iterator last) {
+		using ascii::space;
+		
+		newick_grammar<Iterator> newick_parser;
+		
+		bool r = qi::phrase_parse(first, last, newick_parser, space);
+		if( first != last )
+			return false;
+		return r;
+	}
 };
 
 }
