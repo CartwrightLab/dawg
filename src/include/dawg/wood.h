174f87e78a09a5c43a96d44afd39f8c097418ff6
Reed A. Cartwright <reed@cartwrig.ht>
2009-12-03 08:43:29 +0000

    experimenting with newick parser
    git-svn-id: svn://scit.us/dawg/current@454 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/wood.h
+++ src/include/dawg/wood.h
@@ -8,11 +8,19 @@
 
 #include <boost/scoped_ptr.hpp>
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
+#include <boost/fusion/include/void.hpp>
+#include <boost/spirit/home/phoenix/core/argument.hpp>
+#include <boost/spirit/home/phoenix/stl/algorithm/iteration.hpp>
 
 namespace dawg {
 
 namespace qi = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
+namespace phoenix = boost::phoenix;
 
 template <typename Iterator>
 struct newick_grammar : qi::grammar<Iterator, ascii::space_type> {
@@ -23,23 +31,30 @@ struct newick_grammar : qi::grammar<Iterator, ascii::space_type> {
 		using boost::spirit::lexeme;
 		using boost::spirit::char_;
 		using ascii::space;
+		using boost::spirit::arg_names::_1;
+		using boost::spirit::arg_names::_val;
+		using boost::phoenix::arg_names::arg1;
+		using phoenix::val;
+		using phoenix::for_each;
 		
 		start = node >> ';';
 		node =  tip | inode;
-		tip = label >> -(':' >> double_);
+		tip = label[std::cout << _1 << std::endl] >> -(':' >> double_);
 		inode = '(' >> (node % ',') >> ')' >> -(label || (':' >> double_));
-		label = unquoted | quoted;
-		unquoted = lexeme[+(char_ - (char_("()[]':;,")|space))];
-		quoted = lexeme['\'' >> (*(char_ - '\'') >> *(char_("\'") >> char_("\'") >> *(char_ - '\''))) >> '\''];
+		label %= unquoted | quoted;
+		unquoted %= lexeme[+(char_ - (char_("()[]':;,")|space))];
+		quoted = lexeme['\'' >>
+			*(char_ - '\'')[_val += _1] >> *(char_("\'") >> char_("\'")[_val += "'"] >> *(char_ - '\'')[_val += _1])
+			>> '\''];
 	}
 	
 	qi::rule<Iterator, ascii::space_type> start;
 	qi::rule<Iterator, ascii::space_type> node;
 	qi::rule<Iterator, ascii::space_type> tip;
 	qi::rule<Iterator, ascii::space_type> inode;
-	qi::rule<Iterator, ascii::space_type> label;
-	qi::rule<Iterator, ascii::space_type> unquoted;
-	qi::rule<Iterator, ascii::space_type> quoted;
+	qi::rule<Iterator, std::string(), ascii::space_type> label;
+	qi::rule<Iterator, std::string(), ascii::space_type> unquoted;
+	qi::rule<Iterator, std::string(), ascii::space_type> quoted;
 };
 
 // Represents a phylogenetic tree structure
