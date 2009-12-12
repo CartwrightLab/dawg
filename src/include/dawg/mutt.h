293779fe54271622950925c763e0cbe7c915b4db
Reed A. Cartwright <reed@cartwrig.ht>
2009-12-12 06:51:58 +0000

    added gamma and normal distributions started work on rate_model class new create_random_seed function
    git-svn-id: svn://scit.us/dawg/current@470 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/mutt.h
+++ src/include/dawg/mutt.h
@@ -8,8 +8,10 @@
 #include <cmath>
 #include <cfloat>
 #include <limits>
+#include <ctime>
 
 #include <boost/cstdint.hpp>
+#include <boost/functional/hash.hpp>
 
 #include <dawg/details/mutt.h>
 
@@ -30,40 +32,32 @@ public:
 	// returns random 32-bit number
 	boost::uint32_t rand_uint32() { return gen.rand_uint32(); }
 	// random boolean p prob of success
-	bool rand_bool(double p = 0.5) { return (rand_01() < p); }
+	inline bool rand_bool(double p = 0.5) { return (rand_01() < p); }
 	// random exponential with rate r
-	double rand_exp(double r = 1.0) { return -log(rand_01oc())/r; }
+	inline double rand_exp(double r = 1.0) { return -log(rand_01oc())/r; }
 	
 	// random geometric with succcess rate p in (0,1)
 	// p(1 - p)^(k-1) for k >= 1; q = 1-p
 	inline boost::uint32_t rand_geometric(double q) {
 		return 1+static_cast<uint32_t>(rand_exp(-log(q)));
 	}
+
+	// random zeta with slope z
+	boost::uint32_t rand_zeta(double z);
 	
-	// random zeta distribution with slope z
-	// rejection-inversion method of H\"ormann and Derflinger (1996)
-	// idea borrowed from Indelible
-	// optimizations for usage in Dawg
-	inline boost::uint32_t rand_zeta(double z) {
-		double z1 = 1.0-z;
-		double z2 = 1.0/z1;
-		double s = 2.0-zHi(zH(1.5,z1,z2)-pow(2.0,-z),z1,z2);
-		double Him = zH(4294967296.5,z1,z2);
-		double Hx0 = zH(0.5,z1,z2)-1.0-Him;
-		
-		double U, X,K;
-		for(;;) {
-			U = rand_01(); // [0,1)
-			U = Him+U*Hx0;
-			X = zHi(U,z1,z2);
-			K = floor(X+1.5);
-			if(K-X < s)
-				break;
-			if(U > zH(K-0.5,z1,z2)-pow(K,-z))
-				break;
-		}
-		return static_cast<uint32_t>(K);
+	// random gamma with mean 1 and var b
+	inline double rand_gamma(double b) {
+		rand_gamma(1.0/b, b);
 	}
+	// random gamma with mean ab and var abb
+	double rand_gamma(double a, double b);
+
+	// random normal with mean and sigma
+	inline double rand_normal(double m, double s) {
+		return rand_normal(s)+m;
+	}
+	// random normal with sigma
+	double rand_normal(double sigma);
 	
 private:
 	generator gen;
@@ -74,7 +68,33 @@ private:
 	inline double zHi(double x, double z1, double z2) {
 		return pow(z1*x,z2)-1.0;
 	}
+	
 };
+
+// George Marsaglia's quick but good generator
+// Short Period: 2^32-1
+class shr3 {
+public:
+	shr3() : jsr(1776) { }
+	shr2(boost::uint32_t s) : jsr(s) { }
+	
+	inline boost::uint32_t rand_uint32() {
+		return (jsr^=(jsr<<17), jsr^=(jsr>>13), jsr^=(jsr<<5));
+	}
+	inline void seed(uint32_t s) {
+		jsr = s;
+	}
+private:
+	boost::uinit_32 jsr;
+};
+
+inline boost::uint32_t create_random_seed() {
+	boost::uint32_t v = static_cast<boost::uint32_t>(getpid());
+	v += (v << 15) + (v >> 3); // Spread 5-decimal PID over 32-bit number
+	boost::hash_combine(v, time(NULL));
+	// finish with one round of shr3
+	return (v^=(v<<17), v^=(v>>13), v^=(v<<5));	
+}
 
 } // namespace dawg
 #endif
---
+++ src/include/dawg/rate.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef DAWG_RATE_H
+#define DAWG_RATE_H
+/****************************************************************************
+ *  Copyright (C) 2009 Reed A. Cartwright, PhD <reed@scit.us>               *
+ ****************************************************************************/
+
+namespace dawg {
+
+class indel_model {
+public:
+	
+};
+ 
+} /* namespace dawg */
+ 
+#endif /* DAWG_RATE_H */
