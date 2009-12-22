963f71bfec6507738836eb70204dad232e1901e8
Reed A. Cartwright <reed@cartwrig.ht>
2009-12-22 03:37:14 +0000

    white_space parser lexeme id
    git-svn-id: svn://scit.us/dawg/current@484 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/pile.h
+++ src/include/dawg/pile.h
@@ -34,16 +34,24 @@ namespace pile {
 	typedef std::vector<section_type> pile_type;
 }
 
+template<typename Iterator>
+struct white_space : qi:grammar<Iterator> {
+	white_space() : white_space::base_type(start) {
+		using standard::space; using standard::char_;
+		using qi::eol;
+		start = space | ('#' >> *(char_ - eol) >> eol);
+	}
+	rule<Iterator> space;
+};
 
-template <typename Iterator>
-struct pile_grammar : qi::grammar<Iterator, pile::pile_type(), standard::space_type> {
+template <typename Iterator, typename skip_type>
+struct pile_grammar : qi::grammar<Iterator, pile::pile_type(), skip_type> {
 	
 	pile_grammar() : pile_grammar::base_type(start) {
 		using standard::space; using standard::alnum;
 		using standard::graph; using standard::print;
 		using standard::char_;
-		using qi::raw;
-		using qi::lexeme;
+		using qi::raw; using qi::lexeme; using qi::eol;
 		
 		start = +section;
 		section = section_header || section_body;
@@ -53,31 +61,29 @@ struct pile_grammar : qi::grammar<Iterator, pile::pile_type(), standard::space_t
 		subsection_header = '[' >> -id >> ']';
 		subsection_body = +line;
 		line = id >> '=' >> (pile_string % ',');
-		id = +(alnum | char_("._-"));
+		id = lexeme[+(alnum | char_("._-"));]
 		pile_string     = qqquoted_string |quoted_string | tree_string | bare_string;
-		bare_string     = lexeme[+graph - '"'];
+		bare_string     = lexeme[+graph - char_(",#\"[]=();"];
 		tree_string     = lexeme[char_("(") >> +(char_ - ';') >> char_(";")];
 		quoted_string   = lexeme['"' >> *(print - '"') >> '"'];
-		qqquoted_string = lexeme["\"\"\"" >> *(char_ - "\"\"\"") >> "\"\"\""];
-		
+		qqquoted_string = lexeme["\"\"\"" >> *(char_ - "\"\"\"") >> "\"\"\""];		
 	}
 	
-	qi::rule<Iterator, pile::pile_type(), standard::space_type> start;
-	qi::rule<Iterator, pile::section_type(), standard::space_type> section;
-	qi::rule<Iterator, pile::section_header_type(), standard::space_type> section_header;
-	qi::rule<Iterator, pile::section_body_type(), standard::space_type> section_body;
-	qi::rule<Iterator, pile::subsection_type(), standard::space_type> subsection;
-	qi::rule<Iterator, std::string(), standard::space_type> subsection_header;
-	qi::rule<Iterator, pile::subsection_body_type(), standard::space_type> subsection_body;
-	qi::rule<Iterator, pile::line_type(), standard::space_type> line;
-	qi::rule<Iterator, std::string(), standard::space_type> id;
-	qi::rule<Iterator, std::string(), standard::space_type> pile_string;
-	qi::rule<Iterator, std::string(), standard::space_type> bare_string;
-	qi::rule<Iterator, std::string(), standard::space_type> tree_string;
-	qi::rule<Iterator, std::string(), standard::space_type> quoted_string;
-	qi::rule<Iterator, std::string(), standard::space_type> qqquoted_string;	
+	qi::rule<Iterator, pile::pile_type(), skip_type> start;
+	qi::rule<Iterator, pile::section_type(), skip_type> section;
+	qi::rule<Iterator, pile::section_header_type(), skip_type> section_header;
+	qi::rule<Iterator, pile::section_body_type(), skip_type> section_body;
+	qi::rule<Iterator, pile::subsection_type(), skip_type> subsection;
+	qi::rule<Iterator, std::string(), skip_type> subsection_header;
+	qi::rule<Iterator, pile::subsection_body_type(), skip_type> subsection_body;
+	qi::rule<Iterator, pile::line_type(), skip_type> line;
+	qi::rule<Iterator, std::string(), skip_type> id;
+	qi::rule<Iterator, std::string(), skip_type> pile_string;
+	qi::rule<Iterator, std::string(), skip_type> bare_string;
+	qi::rule<Iterator, std::string(), skip_type> tree_string;
+	qi::rule<Iterator, std::string(), skip_type> quoted_string;
+	qi::rule<Iterator, std::string(), skip_type> qqquoted_string;		
 };
-
 
 } // namespace dawg
 
