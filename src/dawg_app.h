fe1d1a682fb63157e2fc8984eeb64181a5d4784c
Reed A. Cartwright <reed@cartwrig.ht>
2010-01-05 03:36:10 +0000

    missing file
    git-svn-id: svn://scit.us/dawg/current@495 7e5b8c5c-87e2-0310-a459-8d91125b37d5

---
+++ src/dawg_app.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+ *  Copyright (C) 2009 Reed A. Cartwright, PhD <reed@scit.us>               *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ****************************************************************************/
+
+#ifndef DAWG_APP_H
+#define DAWG_APP_H
+
+#include <vector>
+#include <utility>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+/****************************************************************************
+ *    class dawg_app                                                        *
+ ****************************************************************************/
+
+class dawg_app  {
+public:
+	dawg_app(int argc, char *argv[]);
+	virtual ~dawg_app() { }
+	
+	virtual int run();
+	
+	po::options_description desc;	
+	
+	struct args
+	{			
+	// use X-Macros to specify argument variables
+#	define XCMD(lname, sname, desc, type, def) type _V(lname) ;
+#	include "dawg.cmds"
+#	undef XCMD
+	};
+	
+protected:
+	args arg;
+	
+};
+
+namespace boost { namespace program_options {
+template<>
+typed_value<bool>* value(bool* v) {
+	return bool_switch(v);
+}
+}}
+
+#endif
+
