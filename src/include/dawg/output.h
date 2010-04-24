9898ab1947105368701268457179109b2f8ebf72
Reed A. Cartwright <reed@cartwrig.ht>
2010-04-24 06:01:20 +0000

    append and split options implemented
    git-svn-id: svn://scit.us/dawg/current@572 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/output.h
+++ src/include/dawg/output.h
@@ -18,13 +18,18 @@ namespace dawg {
 
 class output {
 public:
-	output() : p_out(NULL), format_id(0), do_op(&output::print_fasta) { }
+	output() : p_out(NULL), format_id(0), do_op(&output::print_aln),
+		split_width(0), app(false), rep(0), split_id_offset(0) { }
 
-	bool open(const char *file_name);
+	bool open(const char *file_name, unsigned int max_rep=0, bool append=false);
 
-	inline void operator()(const alignment& aln) {
-		if(p_out != NULL)
-			(this->*do_op)(aln);
+	inline bool operator()(const alignment& aln) {
+		open_next();
+		if(p_out == NULL)
+			return false;
+		(this->*do_op)(aln);
+		++rep;
+		return true;
 	}
 
 	template<class T>
@@ -47,9 +52,16 @@ private:
 	void print_phylip(const alignment& aln);
 
 protected:
+	bool open_file(const char* file_name);
+	bool open_next();
+
 	std::ostream *p_out;
 	std::ofstream fout;
 	std::size_t format_id;
+	unsigned int rep, split_width;
+	bool app;
+	std::string split_file_name;
+	std::string::size_type split_id_offset;
 };
 
 template<class T>
--- src/lib/output.cpp
+++ src/lib/output.cpp
@@ -12,17 +12,22 @@
 #include <iomanip>
 
 #include <boost/range/iterator_range.hpp>
+#include <boost/spirit/include/karma_generate.hpp>
+#include <boost/spirit/include/karma_uint.hpp>
+#include <boost/spirit/include/karma_right_alignment.hpp>
+#include <boost/spirit/include/karma_char_.hpp>
 
 using namespace dawg;
 using namespace std;
 
-bool dawg::output::open(const char *file_name) {
+bool dawg::output::open(const char *file_name, unsigned int max_rep, bool append) {
 	typedef boost::iterator_range<const char*> cs_range;
-
 	set_format("aln");
+	rep = 0;
 	cs_range format(file_name, file_name);
+	const char *mid = NULL;
 	if(file_name != NULL && file_name[0] != '\0') {
-		const char *mid = strchr(file_name, ':');
+		mid = strchr(file_name, ':');
 #ifdef BOOST_WINDOWS
 		if(mid != NULL && mid != file_name+1) {
 #else
@@ -31,6 +36,8 @@ bool dawg::output::open(const char *file_name) {
 			// format:file
 			format = boost::make_iterator_range(file_name, mid);
 			file_name = mid+1;
+			// find extension point for later
+			mid = strrchr(file_name, '.');
 		} else if((mid = strrchr(file_name, '.')) != NULL) {
 			// file.format
 			format = boost::make_iterator_range(mid+1, strchr(mid+1, '\0'));
@@ -39,25 +46,77 @@ bool dawg::output::open(const char *file_name) {
 			return DAWG_ERROR("unknown output format \'"
 						<< std::string(format.begin(), format.end()) << "\'.");
 		}
-	};
-	if(file_name != NULL && file_name[0] != '\0'
-		&& strcmp(file_name, "-") != 0) {
-		if(fout.is_open())
-			fout.close();
-		fout.open(file_name, ios_base::out|ios_base::trunc);
-		if(!fout.is_open()) {
-			return DAWG_ERROR("unable to open output file \'" << file_name<< "\'.");
+	}
+	if(file_name != NULL && file_name[0] != '\0' && strcmp(file_name, "-") != 0) {
+		// set append option before we open the file
+		app = append;
+		// find how wide the id number must be
+		unsigned int m = max_rep;
+		split_width = (m == 0) ? 0 : (m < 10) ? 1 : (m < 100) ? 2 : (m < 1000) ? 3 :
+			(m < 10000) ? 4 : (m < 100000) ? 5 : (m < 1000000) ? 6 :
+			(m < 10000000) ? 7 : (m < 100000000) ? 8 :
+			(m < 1000000000) ? 9 : 10;
+		// open our omnibus output if desired
+		if(split_width == 0) {
+			if(!open_file(file_name))
+				return DAWG_ERROR("unable to open output file \'" << file_name<< "\'.");
+		} else {
+			// setup output_filename
+			if(mid == NULL) {
+				split_file_name.assign(file_name);
+				split_file_name.append(1, '-');
+				split_file_name.append(split_width, '0');
+				split_id_offset = split_file_name.size()-split_width;
+			} else {
+				split_file_name.assign(file_name, mid);
+				split_file_name.append(1, '-');
+				split_file_name.append(split_width, '0');
+				split_file_name.append(mid);
+				split_id_offset = (mid-file_name)+1;
+			}
 		}
-		set_ostream(fout);
 	} else {
+		// turn off appending and spliting
+		app = false;
+		split_width = 0;
 		set_ostream(cout);
 	}
 	return true;
 }
 
+bool dawg::output::open_file(const char* file_name) {
+	if(fout.is_open())
+		fout.close();
+	ios_base::openmode om = ios_base::out | (app ? ios_base::app : ios_base::trunc);
+	fout.open(file_name, om);
+	if(!fout.is_open()) {
+		set_ostream(NULL);
+		return false;
+	}
+	set_ostream(fout);
+	return true;
+}
+
+bool dawg::output::open_next() {
+	// no need to open next file
+	if(split_width == 0)
+		return true;
+	using boost::spirit::karma::generate;
+	using boost::spirit::karma::uint_;
+	using boost::spirit::karma::lit;
+	using boost::spirit::karma::right_align;
+
+	string::iterator it = split_file_name.begin()+split_id_offset;
+	generate(it, right_align(split_width,lit('0'))[uint_], rep);
+	
+	if(!open_file(split_file_name.c_str()))
+		return DAWG_ERROR("unable to open output file \'" << split_file_name << "\'.");
+	return true;
+}
+
 void dawg::output::print_fasta(const alignment& aln) {
 	ostream &out = *p_out;
-	
+
 	foreach(const alignment::value_type& v, aln) {
 		out << ">" << v.label << endl;
 
@@ -75,7 +134,7 @@ void dawg::output::print_fasta(const alignment& aln) {
 void dawg::output::print_aln(const alignment& aln) {
 	ostream &out = *p_out;
 	out << "CLUSTAL multiple sequence alignment (Created by "
-		<< PACKAGE_STRING << ")\n\n" << endl;
+		<< PACKAGE_STRING << ")\n" << endl;
 
 	// find alignment length
 	string::size_type u=0, len = aln[0].seq.length();
@@ -85,7 +144,7 @@ void dawg::output::print_aln(const alignment& aln) {
 			out.write(&v.seq[u], 60);
 			out << endl;
 		}
-		out << '\n' << endl;
+		out << endl;
 	}
 	len = len-u;
 	foreach(const alignment::value_type& v, aln) {
@@ -93,7 +152,7 @@ void dawg::output::print_aln(const alignment& aln) {
 		out.write(&v.seq[u], len);
 		out << endl;
 	}
-	out << '\n' << endl;
+	out << endl;
 }
 
 void dawg::output::print_poo(const alignment& aln) {
@@ -122,8 +181,8 @@ void dawg::output::print_nexus(const alignment& aln) {
 	ostream &out = *p_out;
 	
 	static char datatypes[][10] = {
-		"DNA", "RNA", "PROTEIN", "PROTEIN",
-		"DNA", "RNA", "PROTEIN", "PROTEIN"
+		"DNA", "RNA", "PROTEIN", "DNA",
+		"DNA", "RNA", "PROTEIN", "DNA"
 	};
 	
 	// TODO: Move this to the block support routine
