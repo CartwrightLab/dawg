8092f31dec8d9c0bf6f509b8afa21de596f0ae3c
Reed A. Cartwright <reed@cartwrig.ht>
2010-04-28 23:24:38 +0000

    residue model
    git-svn-id: svn://scit.us/dawg/current@576 7e5b8c5c-87e2-0310-a459-8d91125b37d5

---
+++ src/include/dawg/resid.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef DAWG_RESID_H
+#define DAWG_RESID_H
+/****************************************************************************
+ *  Copyright (C) 2010 Reed A. Cartwright, PhD <reed@scit.us>               *
+ ****************************************************************************/
+
+#include <dawg/residue.h>
+#include <dawg/subst.h>
+#include <dawg/rate.h>
+
+namespace dawg {
+
+class resid_model {
+public:
+	// create function
+	
+	// sequence type
+	
+	// creating a root
+	
+	// random residues
+	
+	// evolve residues
+	
+	// constant or variable residue widths
+	
+	// indel widths as well?
+};
+
+} // namespace dawg
+
+#endif // DAWG_RESID_H
+
