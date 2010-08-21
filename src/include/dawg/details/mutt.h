dab6f35453e9fbce866f59f0dac76f0aba2f9039
Reed A. Cartwright <reed@cartwrig.ht>
2010-08-21 00:06:42 +0000

    use exp zig to increase speed
    git-svn-id: svn://scit.us/dawg/current@589 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/include/dawg/details/mutt.h
+++ src/include/dawg/details/mutt.h
@@ -5,7 +5,17 @@
  *  Copyright (C) 2009 Reed A. Cartwright, PhD <reed@scit.us>               *
  ****************************************************************************/
 
+#define __STDC_CONSTANT_MACROS 1
+#include <boost/cstdint.hpp>
+#include <boost/functional/hash.hpp>
+#include <cmath>
+
 // include stuff for DSFMT
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+#	define DSFMT_UINT32_DEFINED 1
+using boost::uint32_t;
+using boost::uint64_t;
+#endif
 #define DSFMT_MEXP 19937
 #define DSFMT_DO_NOT_USE_OLD_NAMES
 extern "C" {
@@ -18,7 +28,12 @@ struct dsfmt_mutt_gen {
 	double rand_01()  { return dsfmt_genrand_close_open(&state);	}
 	double rand_01oc() { return dsfmt_genrand_open_close(&state); }
 	double rand_01oo() { return dsfmt_genrand_open_open(&state); }
-	uint32_t rand_uint32() { return dsfmt_genrand_uint32(&state); }
+	boost::uint32_t rand_uint32() { return dsfmt_genrand_uint32(&state); }
+	boost::uint64_t rand_uint64() { 
+		uint64_t a = rand_uint32();
+		uint64_t b = rand_uint32();
+		return (a << 32) | b;
+	}
 	void seed(uint32_t x) { dsfmt_init_gen_rand(&state, x); }
 	template<int _N>
 	void seed(uint32_t (&x)[_N]) {
@@ -27,7 +42,7 @@ struct dsfmt_mutt_gen {
 	template<typename _It>
 	void seed(_It first, _It last) {
 		std::size_t sz = last-first;
-		uint32_t *p = new uint32_t[sz];
+		boost::uint32_t *p = new boost::uint32_t[sz];
 		std::copy(first, last, p);
 		dsfmt_init_by_array(&state, p, sz);
 		delete[] p;
@@ -37,6 +52,182 @@ private:
 	dsfmt_t state;
 };
 
+// George Marsaglia's quick but good generator
+// http://www.jstatsoft.org/v08/i14/paper
+// 64-bit period
+// 0,0 never seen
+struct shr3a_mutt_gen {
+	inline boost::uint32_t rand_uint32() {
+		uint32_t t = x^(x<<2);
+		x=y;
+		y=(y^(y>>3))^(t^(t>>7));
+		return y;
+	}
+	inline boost::uint64_t rand_uint64() {
+		uint64_t a = rand_uint32();
+		uint64_t b = rand_uint32();
+		return (a << 32) | b;
+	}
+	// doubles with 53-bits worth of precision
+	inline double rand_01() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return u/4503599627370496.0;
+	}
+	inline double rand_01oo() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370497.0);
+	}
+	inline double rand_01oc() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370496.0);
+	}
+	inline void seed(boost::uint32_t xx) { 
+		y = xx;
+		// 1 round of 32-bit shr3 to fill in the rest
+		x = ((xx^=(xx<<17), xx^=(xx>>13), xx^=(xx<<5)));
+	}
+	template<int _N>
+	inline void seed(boost::uint32_t (&xx)[_N]) {
+		seed(&xx[0],&xx[_N]);
+	}
+	template<typename _It>
+	inline void seed(_It first, _It last) {
+		if(first == last)
+			return; // nothing to do
+		y = *first++;
+		if(first == last) {
+			seed(y);  // single value
+			return;
+		}
+		// use hash to make every element matter
+		std::size_t h = static_cast<std::size_t>(*first++);
+		boost::hash_range(h, first, last);
+		x = static_cast<boost::uint32_t>(h);
+	}	
+	
+	private:
+		boost::uint32_t x,y;
+};
+
+// 0 never seen
+struct shr3b_mutt_gen {
+	inline boost::uint32_t rand_uint32() {
+		return static_cast<boost::uint32_t>(rand_uint64());
+	}
+	inline boost::uint64_t rand_uint64() {
+		y ^= (y << 13);
+		y ^= (y >> 7);
+		y ^= (y << 17);
+		return y;
+	}
+	// doubles with 53-bits worth of precision
+	inline double rand_01() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return u/4503599627370496.0;
+	}
+	inline double rand_01oo() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370497.0);
+	}
+	inline double rand_01oc() {
+		uint64_t u = rand_uint64() & UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370496.0);
+	}
+	inline void seed(boost::uint32_t xx) { 
+		y = xx;
+	}
+	template<int _N>
+	inline void seed(boost::uint32_t (&xx)[_N]) {
+		seed(&xx[0],&xx[_N]);
+	}
+	template<typename _It>
+	inline void seed(_It first, _It last) {
+		if(first == last)
+			return; // nothing to do
+		y = *first++;
+		if(first == last)
+			return;
+		// use hash to make every element matter
+		std::size_t h = static_cast<std::size_t>(*first++);
+		boost::hash_range(h, first, last);
+		y |= (static_cast<boost::uint64_t>(h) << 32);
+		
+	}
+	
+	private:
+		boost::uint64_t y;
+};
+
+// Ziggurat method for rand exponential value
+// Memory hog, but really really fast
+class rand_exp_ziggurat {
+public:
+
+	template<class _R>	
+	inline double operator()(_R &rng) const {
+		static const double r = 7.69711747013104972;
+		boost::uint64_t a = rng.rand_uint64();
+		int b = a & 255;
+		a >>= 12;
+		if( a <= k[b])
+			return a*w[b];
+		do {
+			if(b == 0)
+				return r-log(unif01open(rng.rand_uint64()));
+			double x = a*w[b];
+			// we can cache f[b-1]-f[b], but it should be minor
+			if(f[b]+unif01(rng.rand_uint64())*(f[b-1]-f[b]) < exp(-x) )
+				return x;
+			a = rng.rand_uint64();
+			b = a & 255;
+			a >>= 12;
+		} while(a > k[b]);
+		
+		return a*w[b];
+	}
+	template<class _R>
+	inline double inv(_R &rng) const {
+		return log(unif01open(rng.rand_uint64()));
+	}
+	rand_exp_ziggurat() {
+		static const double v = 0.0039496598225815571993;
+		static const double m = 4503599627370496.0; // 2^52
+		
+		double d=7.69711747013104972, t=d, q;
+		
+		q = v/exp(-d);
+		k[0]=(d/q)*m;
+		k[1]=0;
+
+		w[0]=q/m;
+		w[255]=d/m;
+
+		f[0]=1;
+		f[255]=q=exp(-d);
+
+		for(int i=254; i>=1; i--) {
+			d=-log(v/d+q);
+			k[i+1] = (d/t)*m;
+			t=d;
+			f[i]=q=exp(-d);
+			w[i]=d/m;
+		}
+	}
+	
+protected:
+	static double inline unif01open(boost::uint64_t u) {
+		u &= UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370497.0);
+	}
+
+	static double inline unif01(boost::uint64_t u) {
+		u &= UINT64_C(4503599627370495);
+		return u/4503599627370496.0;
+	}
+
+	double w[256], f[256];
+	boost::uint64_t k[256];
+};
 
 }} /* namespace dawg::details */
 
--- src/include/dawg/mutt.h
+++ src/include/dawg/mutt.h
@@ -13,14 +13,7 @@
 #include <vector>
 
 #include <boost/config.hpp>
-#include <boost/cstdint.hpp>
 #include <boost/functional/hash.hpp>
-
-#if defined(_MSC_VER) || defined(__BORLANDC__)
-#	define DSFMT_UINT32_DEFINED 1
-using boost::uint32_t;
-using boost::uint64_t;
-#endif
 
 #include <dawg/details/mutt.h>
 
@@ -32,7 +25,9 @@ namespace dawg {
 
 class mutt {
 public:
-	typedef details::dsfmt_mutt_gen generator;
+	//typedef details::dsfmt_mutt_gen generator;
+	//typedef details::shr3a_mutt_gen generator;
+	typedef details::shr3b_mutt_gen generator;
 	
 	inline void seed(uint32_t s) { 
 		_seed.assign(1, s);
@@ -63,7 +58,9 @@ public:
 	// random boolean p prob of success
 	inline bool rand_bool(double p = 0.5) { return (rand_01() < p); }
 	// random exponential with rate r
-	inline double rand_exp(double r = 1.0) { return -log(rand_01oc())/r; }
+	inline double rand_exp(double r = 1.0) { return rand_exp_zig()/r; }
+	inline double rand_exp_zig();
+	inline double rand_exp_inv();
 	
 	// random geometric with succcess rate p in (0,1)
 	// p(1 - p)^(k-1) for k >= 1; q = 1-p
@@ -101,31 +98,30 @@ private:
 	generator gen;
 	std::vector<uint32_t> _seed;
 	
-	inline double zH(double x, double z1, double z2) {
+	static const boost::uint64_t ek[256];
+	static const double ew[256], ef[256];
+	
+	static inline double zH(double x, double z1, double z2) {
 		return pow(x+1.0, z1)*z2;
 	}
-	inline double zHi(double x, double z1, double z2) {
+	static inline double zHi(double x, double z1, double z2) {
 		return pow(z1*x,z2)-1.0;
 	}
+
+	static double inline unif01open(boost::uint64_t u) {
+		u &= UINT64_C(4503599627370495);
+		return ((u+1)/4503599627370497.0);
+	}
+
+	static double inline unif01(boost::uint64_t u) {
+		u &= UINT64_C(4503599627370495);
+		return u/4503599627370496.0;
+	}	
 	
 };
 
 // George Marsaglia's quick but good generator
 // Short Period: 2^32-1
-class shr3 {
-public:
-	shr3() : jsr(1776) { }
-	shr3(boost::uint32_t s) : jsr(s) { }
-	
-	inline boost::uint32_t rand_uint32() {
-		return (jsr^=(jsr<<17), jsr^=(jsr>>13), jsr^=(jsr<<5));
-	}
-	inline void seed(uint32_t s) {
-		jsr = s;
-	}
-private:
-	boost::uint32_t jsr;
-};
 
 inline boost::uint32_t create_random_seed() {
 	std::size_t v = static_cast<std::size_t>(getpid());
@@ -138,6 +134,33 @@ inline boost::uint32_t create_random_seed() {
 inline mutt::mutt() {
 	seed(create_random_seed());
 }
+
+inline double mutt::rand_exp_zig() {
+	static const double r = 7.69711747013104972;
+	boost::uint64_t a = gen.rand_uint64();
+	int b = a & 255;
+	a >>= 12;
+	if( a <= ek[b])
+		return a*ew[b];
+	do {
+		if(b == 0)
+			return r-log(unif01open(gen.rand_uint64()));
+		double x = a*ew[b];
+		// we can cache ef[b-1]-ef[b], but it should be minor
+		if(ef[b]+unif01(gen.rand_uint64())*(ef[b-1]-ef[b]) < exp(-x) )
+			return x;
+		a = gen.rand_uint64();
+		b = a & 255;
+		a >>= 12;
+	} while(a > ek[b]);
+	
+	return a*ew[b];
+}
+
+inline double mutt::rand_exp_inv() {
+	return -log(unif01open(gen.rand_uint64()));
+}
+
 
 } // namespace dawg
 #endif
