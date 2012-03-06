4e8625785feb6178ad5254837bc0d67ba7908962
Reed A. Cartwright <reed@cartwrig.ht>
2012-03-06 23:34:17 +0000

    ibid
    git-svn-id: svn://scit.us/dawg/current@639 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/lib/matic.cpp
+++ src/lib/matic.cpp
@@ -1,10 +1,6 @@
 /****************************************************************************
  *  Copyright (C) 2009 Reed A. Cartwright, PhD <reed@scit.us>               *
  ****************************************************************************/
-#ifdef HAVE_CONFIG_H
-#	include "config.h"
-#endif
-
 #include <dawg/matic.h>
 #include <dawg/residue.h>
 #include <dawg/log.h>
