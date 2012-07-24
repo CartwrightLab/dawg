1e5cfad1ca51fe4f89f945c67f19a2ebdf490c75
Reed A. Cartwright <reed@cartwrig.ht>
2012-07-24 08:11:34 +0000

    broken vose method, save for later study
    git-svn-id: svn://scit.us/dawg/current@680 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/lib/models.cpp
+++ src/lib/models.cpp
@@ -13,8 +13,6 @@ inline boost::uint64_t uint64_bound(double bound) {
 	return static_cast<boost::uint64_t>(ceil(bound*9007199254740991.0)) << 11;
 }
 
-UINT64_C(18446744073709551615)
-
 // vose method for creating alias tables
 // http://www.keithschwarz.com/darts-dice-coins/
 void alias_table_64(const double *pp, boost::uint64_t *q, boost::uint32_t *a) {
@@ -27,61 +25,78 @@ void alias_table_64(const double *pp, boost::uint64_t *q, boost::uint32_t *a) {
 	for(int i=0;i<64;++i) {
 		p[i] = pp[i]*f*64.0;
 	}
-	
-	std::size_t m=0,g=0;
-	
-	for(g=0;g<64 && p[g] < 1.0;++g)
-		/*noop*/
-	for(m=g; m-g < 64 && p[m%64] >= 1.0;++m)
-		/*noop*/
-	const std::size_t mm = m%64;
-	size_t n = 
-	
-	while(g < 64 && n < 64) {
-		
-	}
-	for(;g<64;++g) {
-		q[g] = UINT64_C(18446744073709551615);
-		a[g] = g;
-	}
-	for(; m-mm < 64; ++m) {
-		
-	}
-
-	
-	while(g < 64) {
-		if(p[g] < 1.0) {
-			g += 1; continue;
-		}
-		if(p[m] >= 1.0) {
-			m = (m+1)%64;
-		}
-	}	
-	for(;g < 64;++g) {
+#if 0	
+	// find the starting pair of sites.
+	int g,m,n,m0;
+	for(g=0; g < 64 && p[g] < 1.0; ++g)
+		/*noop*/;
+	for(n=0; n < 64 && p[(g+n)%64] >= 1.0; ++n)
+		/*noop*/;
+	// save the initial m, check to see if m-vector is empty
+	m0 = g+n;
+	n = m0+(n & 64);
+	while(g < 64 && n < m0+64) {
+		m = n%64;
 		q[m] = uint64_bound(p[m]);
 		a[m] = g;
-		p[g] = (p[g]+p[m])-d;
-				
+		p[g] = (p[g]+p[m])-1.0;
+		// find next sites as needed
+		for(;g < 64 && p[g] < 1.0; ++g)
+			/*noop*/;
+		for(++n;n < m0+64 && p[n%64] >= 1.0; ++n)
+			/*noop*/;
 	}
-	
-
+	for(;g < 64; ++g) {
+		if(p[g] >= 1.0) {
+			q[g] = UINT64_C(18446744073709551615);
+			a[g] = g;
+		}
+	}
+	for(;n < m0+64; ++n) {
+		g = n%64;
+		if(p[g] < 1.0) {
+			q[g] = UINT64_C(18446744073709551615);
+			a[g] = g;
+		}
+	}
+#else
+	std::vector<int> sm,lg;
+	for(int i=63;i>=0;--i) {
+		if(p[i] < 1.0) {
+			sm.push_back(i);
+		} else {
+			lg.push_back(i);
+		}
+	}
 	while(!sm.empty() && !lg.empty()) {
 		int m = sm.back(); sm.pop_back();
-		int g = lg.back(); lg.pop_back();
+		int g = lg.back();
 		q[m] = uint64_bound(p[m]);
 		a[m] = g;
-		p[g] = (p[g]+p[m])-d;
-		if(p[g] < d)
+		p[g] = (p[g]+p[m])-1.0;
+		if(p[g] < 1.0) {
 			sm.push_back(g);
-		else
-			lg.push_back(g);
+			lg.pop_back();
+		}
 	}
-
-	
+	for(int i=0;i<sm.size();++i) {
+		int m = sm[i];
+		q[m] = std::numeric_limits<boost::uint64_t>::max();
+		a[m] = m;
+	}
+	for(int i=0;i<lg.size();++i) {
+		int g = lg[i];
+		q[g] = std::numeric_limits<boost::uint64_t>::max();
+		a[g] = g;
+	}
+#endif	
 }
  
 bool dawg::subst_model::create_alias_tables() {
 	alias_table_64(&freqs[0], &stat_dist_p[0], &stat_dist_a[0]);
+	for(int i=0;i<64;++i) {
+		printf("%u %llu %u\n", i, stat_dist_p[i], stat_dist_a[i]);
+	}
 	for(int i=0;i<64;++i)
 		alias_table_64(&table[i][0], &mutation_p[i][0], &mutation_a[i][0]);
 	return true;
