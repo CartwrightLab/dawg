85e5d6a0716be351cd185ce40d638c1b1f8c14f0
Reed A. Cartwright <reed@cartwrig.ht>
2012-07-24 02:44:38 +0000

    working on removing vectors in alias_table
    git-svn-id: svn://scit.us/dawg/current@679 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/lib/models.cpp
+++ src/lib/models.cpp
@@ -3,42 +3,86 @@
  ****************************************************************************/
 
 #include <dawg/subst.h>
+#include <cassert>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/sum_kahan.hpp>
  
- // vose method for creating alias tables
- // http://www.keithschwarz.com/darts-dice-coins/
- void alias_table_64(const double *p, boost::uint64_t *q, boost::uint32_t *a) {
-	double np[64];
-	std::vector<int> sm, lg;
-	double d = 0.0;
-	for(int i=0;i<64;++i)
-		d += p[i];
-	d = 64.0/d;
-	for(int i=0;i<64;++i) {
-		q[i] = std::numeric_limits<boost::uint64_t>::max();
-		a[i] = i;
-		np[i] = p[i]*d;
-		if(np[i] < 1.0) {
-			sm.push_back(i);
-		} else {
-			lg.push_back(i);
-		}
+inline boost::uint64_t uint64_bound(double bound) {
+	assert(0.0 <= bound && bound < 1.0);
+	return static_cast<boost::uint64_t>(ceil(bound*9007199254740991.0)) << 11;
+}
+
+UINT64_C(18446744073709551615)
+
+// vose method for creating alias tables
+// http://www.keithschwarz.com/darts-dice-coins/
+void alias_table_64(const double *pp, boost::uint64_t *q, boost::uint32_t *a) {
+	double d=0.0, f=0.0, p[64];
+	// use pairs-summation to control error
+	for(int i=0;i<64;i+=2) {
+		d += pp[i]; f += pp[i+1];
 	}
+	f = 1.0/(d+f);
+	for(int i=0;i<64;++i) {
+		p[i] = pp[i]*f*64.0;
+	}
+	
+	std::size_t m=0,g=0;
+	
+	for(g=0;g<64 && p[g] < 1.0;++g)
+		/*noop*/
+	for(m=g; m-g < 64 && p[m%64] >= 1.0;++m)
+		/*noop*/
+	const std::size_t mm = m%64;
+	size_t n = 
+	
+	while(g < 64 && n < 64) {
+		
+	}
+	for(;g<64;++g) {
+		q[g] = UINT64_C(18446744073709551615);
+		a[g] = g;
+	}
+	for(; m-mm < 64; ++m) {
+		
+	}
+
+	
+	while(g < 64) {
+		if(p[g] < 1.0) {
+			g += 1; continue;
+		}
+		if(p[m] >= 1.0) {
+			m = (m+1)%64;
+		}
+	}	
+	for(;g < 64;++g) {
+		q[m] = uint64_bound(p[m]);
+		a[m] = g;
+		p[g] = (p[g]+p[m])-d;
+				
+	}
+	
+
 	while(!sm.empty() && !lg.empty()) {
 		int m = sm.back(); sm.pop_back();
 		int g = lg.back(); lg.pop_back();
-		q[m] = (static_cast<boost::uint64_t>(np[m]*9007199254740991.0) << 11) | 0x7FF;
+		q[m] = uint64_bound(p[m]);
 		a[m] = g;
-		np[g] = (np[g]+np[m])-1.0;
-		if(np[g] < 1.0)
+		p[g] = (p[g]+p[m])-d;
+		if(p[g] < d)
 			sm.push_back(g);
 		else
 			lg.push_back(g);
 	}
- }
+
+	
+}
  
- bool dawg::subst_model::create_alias_tables() {
+bool dawg::subst_model::create_alias_tables() {
 	alias_table_64(&freqs[0], &stat_dist_p[0], &stat_dist_a[0]);
 	for(int i=0;i<64;++i)
 		alias_table_64(&table[i][0], &mutation_p[i][0], &mutation_a[i][0]);
 	return true;
- }
+}
