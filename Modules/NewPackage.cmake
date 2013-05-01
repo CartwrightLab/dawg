9880598e1d4694600cfc7b61d5e63d523b89a382
Reed A. Cartwright <reed@cartwrig.ht>
2013-05-01 22:20:09 +0000

    added more NewPackage logic
    git-svn-id: svn://scit.us/dawg/current@713 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- Modules/NewPackage.cmake
+++ Modules/NewPackage.cmake
@@ -5,11 +5,13 @@ if(NOT DEFINED NEW_PACKAGE_NAME)
 	message(WARNING "NEW_PACKAGE_NAME has defaulted to 'software'.")
 endif()
 
-if(DEFINED NEW_PACKAGE_VERSION)
-	set(NEW_PACKAGE_VERSION ${NEW_PACKAGE_VERSION})
-else()
+if(NOT DEFINED NEW_PACKAGE_VERSION)
 	set(NEW_PACKAGE_VERSION "0.1")
 	message(WARNING "NEW_PACKAGE_VERSION has defaulted to '0.1'.")
+endif()
+
+if(NOT DEFINED NEW_PACKAGE_VERSION_FILE)
+	set(NEW_PACKAGE_VERSION_FILE "version.h")
 endif()
 
 # for now use svnversion
@@ -30,19 +32,19 @@ if(Subversion_SVNVERSION_EXECUTABLE)
 	     ERROR_QUIET
 	)
 	if(SVN_OUTPUT MATCHES "^[0-9]")
-		string(REGEX REPLACE "(-r[0-9].*)$" ""
+		string(REGEX REPLACE "(-rUnknown|-r[0-9].*)$" "-r${SVN_OUTPUT}"
 			NEW_PACKAGE_VERSION "${NEW_PACKAGE_VERSION}")
-		set(NEW_PACKAGE_VERSION "${NEW_PACKAGE_VERSION}-r${SVN_OUTPUT}")
 	endif()
+	string(REPLACE ":" "_" NEW_PACKAGE_VERSION "${NEW_PACKAGE_VERSION}")
 endif()
 
 ################################################################################
 # Write helper files to the binary directory
 
-file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/np/")
+file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/newpkg/")
 
 #### version.cmake ####
-FILE(WRITE "${CMAKE_BINARY_DIR}/np/version.cmake" "
+FILE(WRITE "${CMAKE_BINARY_DIR}/newpkg/version.cmake" "
 set(NEW_PACKAGE_VERSION \"${NEW_PACKAGE_VERSION}\")
 set(NEW_PACKAGE_NAME \"${NEW_PACKAGE_NAME}\")
 
@@ -53,25 +55,26 @@ if(${SVNVERSION_FOUND})
 	     ERROR_QUIET
 	)
 	if(SVN_OUTPUT MATCHES \"^[0-9]\")
-		string(REGEX REPLACE \"(-r[0-9].*)\$\" \"\"
+		string(REGEX REPLACE \"(-rUnknown|-r[0-9].*)\$\" \"-r\${SVN_OUTPUT}\"
 			NEW_PACKAGE_VERSION \"\${NEW_PACKAGE_VERSION}\")
-		set(NEW_PACKAGE_VERSION \"\${NEW_PACKAGE_VERSION}-r\${SVN_OUTPUT}\")
+#		set(NEW_PACKAGE_VERSION \"\${NEW_PACKAGE_VERSION}-r\${SVN_OUTPUT}\")
 	endif()
+	string(REPLACE \":\" \"_\" NEW_PACKAGE_VERSION \"\${NEW_PACKAGE_VERSION}\")
 endif()
 
 # Prefer .pkg
-if(EXISTS ${CMAKE_SOURCE_DIR}/version.h.newpkg)
-	set(INFILE ${CMAKE_SOURCE_DIR}/version.h.newpkg)
+if(EXISTS ${CMAKE_SOURCE_DIR}/${NEW_PACKAGE_VERSION_FILE}.newpkg)
+	set(INFILE ${CMAKE_SOURCE_DIR}/${NEW_PACKAGE_VERSION_FILE}.newpkg)
 else()
-	set(INFILE ${CMAKE_BINARY_DIR}/np/version.h.in)
+	set(INFILE ${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}.in)
 endif()
 
-configure_file(\${INFILE} ${CMAKE_BINARY_DIR}/np/version.h @ONLY)
+configure_file(\${INFILE} ${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE} @ONLY)
 ")
 ####
 
 #### version.h.in ####
-file(WRITE "${CMAKE_BINARY_DIR}/np/version.h.in"
+file(WRITE "${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}.in"
 "#pragma once
 #ifndef NEW_PACKAGE_VERSION_H
 #define NEW_PACKAGE_VERSION_H
@@ -91,18 +94,18 @@ SET(NEW_PACKAGE_VERSION \"\@NEW_PACKAGE_VERSION\@\")
 ####
 
 #### new_install.cmake ####
-file(WRITE "${CMAKE_BINARY_DIR}/np/new_install.cmake" "
+file(WRITE "${CMAKE_BINARY_DIR}/newpkg/install.cmake" "
 if(NOT CPACK_INSTALL_CMAKE_PROJECTS)
-	if(EXISTS \"${CMAKE_BINARY_DIR}/np/version.h\")
+	if(EXISTS \"${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}\")
 		execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-			\"${CMAKE_BINARY_DIR}/np/version.h\"
-	        \"\${CMAKE_INSTALL_PREFIX}/version.h.newpkg\"
+			\"${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}\"
+	        \"\${CMAKE_INSTALL_PREFIX}/${NEW_PACKAGE_VERSION_FILE}.newpkg\"
 		)
 	else()
 		set(NEW_PACKAGE_NAME \"\${CPACK_PACKAGE_NAME}\")
 		set(NEW_PACKAGE_VERSION \"\${CPACK_PACKAGE_VERSION}\")
-		configure_file(\"${CMAKE_BINARY_DIR}/np/version.h.in\"
-			\"\${CMAKE_INSTALL_PREFIX}/version.h.newpkg\"
+		configure_file(\"${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}.in\"
+			\"\${CMAKE_INSTALL_PREFIX}/${NEW_PACKAGE_VERSION_FILE}.newpkg\"
 			@ONLY)
 	endif()
 	file(MAKE_DIRECTORY \"\${CMAKE_INSTALL_PREFIX}/build\")
@@ -111,8 +114,56 @@ endif()
 ")
 ####
 
+#### new_package.cmake ####
+file(WRITE "${CMAKE_BINARY_DIR}/newpkg/new_package.cmake" "
+INCLUDE(CPackConfig.cmake)
+
+SET(NEW_PACKAGE_VERSION \"\${CPACK_PACKAGE_VERSION}\")
+SET(NEW_PACKAGE_NAME \"\${CPACK_PACKAGE_NAME}\")
+
+INCLUDE(\"${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}\")
+
+# Some files use directories that contain the version number
+# Update as needed
+IF(NEW_PACKAGE_VERSION MATCHES \"^([0-9]+)\\\\.\")
+	SET(ENV{NEW_PACKAGE_INSTALL_DIR_VERSION} \${CMAKE_MATCH_1})
+ENDIF()
+
+EXECUTE_PROCESS(COMMAND \${CMAKE_CPACK_COMMAND}
+	-D NEW_PACKAGE=TRUE
+	-D CPACK_PACKAGE_FILE_NAME=\${NEW_PACKAGE_NAME}-\${NEW_PACKAGE_VERSION}-\${CPACK_SYSTEM_NAME}
+	--config CPackConfig.cmake
+	-P \"\${NEW_PACKAGE_NAME}\"
+	-R \"\${NEW_PACKAGE_VERSION}\"
+	TIMEOUT 3600
+	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+)
+")
+####
+
+#### new_package_source.cmake ####
+file(WRITE "${CMAKE_BINARY_DIR}/newpkg/new_package_source.cmake" "
+INCLUDE(CPackSourceConfig.cmake)
+
+SET(NEW_PACKAGE_VERSION \"\${CPACK_PACKAGE_VERSION}\")
+SET(NEW_PACKAGE_NAME \"\${CPACK_PACKAGE_NAME}\")
+
+INCLUDE(\"${CMAKE_BINARY_DIR}/${NEW_PACKAGE_VERSION_FILE}\")
+
+EXECUTE_PROCESS(COMMAND \${CMAKE_CPACK_COMMAND}
+	-D NEW_PACKAGE=TRUE
+	-D CPACK_PACKAGE_FILE_NAME=\${NEW_PACKAGE_NAME}-\${NEW_PACKAGE_VERSION}-\${CPACK_SYSTEM_NAME}
+	--config CPackSourceConfig.cmake
+	-P \"\${NEW_PACKAGE_NAME}\"
+	-R \"\${NEW_PACKAGE_VERSION}\"
+	TIMEOUT 3600
+	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+)
+")
+####
+
 ################################################################################
-SET(CPACK_INSTALL_SCRIPT "${CMAKE_BINARY_DIR}/np/new_install.cmake")
+SET(CPACK_INSTALL_SCRIPT "${CMAKE_BINARY_DIR}/newpkg/install.cmake")
 
 install(CODE "
 if(DEFINED ENV{NEW_PACKAGE_INSTALL_DIR_VERSION})
@@ -130,19 +181,22 @@ endif()
 
 ADD_CUSTOM_TARGET(version
     COMMAND ${CMAKE_COMMAND}
-    	-P ${CMAKE_BINARY_DIR}/np/version.cmake
+    	 -P ${CMAKE_BINARY_DIR}/newpkg/version.cmake
 )
 
 ADD_CUSTOM_TARGET(new_package
 	COMMAND ${CMAKE_COMMAND} 
-		-P ${CMAKE_BINARY_DIR}/new_package_package.cmake
+		 -P ${CMAKE_BINARY_DIR}/newpkg/new_package.cmake
 )
 
 ADD_CUSTOM_TARGET(new_package_source
 	COMMAND ${CMAKE_COMMAND} 
-		-P ${CMAKE_BINARY_DIR}/new_package_source.cmake
+		 -P ${CMAKE_BINARY_DIR}/newpkg/new_package_source.cmake
 )
 
 ADD_DEPENDENCIES(new_package version)
 ADD_DEPENDENCIES(new_package_source version)
+
+set(CPACK_PACKAGE_NAME    "${NEW_PACKAGE_NAME}")
+set(CPACK_PACKAGE_VERSION "${NEW_PACKAGE_VERSION}")
 
