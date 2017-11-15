ce5da89e06d84022846e675d4df15737480e552c
zmertens <zmertens@asu.edu>
2017-11-15 16:08:27 -0700

    Dawg links to boost python \ Have symbol mangling issue similar to cython issue

--- boost.python/dawg.cpp
+++ boost.python/dawg.cpp
@@ -11,63 +11,94 @@
 
 class DawgWalker {
 public:
-    void parse() {
-        // std::vector<unsigned char> modifiedInstructionChars;
-    	// input.parse(modifiedInstructionChars.begin(), modifiedInstructionChars.end());
+    DawgWalker();
+    DawgWalker(const std::string &in,
+        const std::string &out,
+        const unsigned int r,
+        const unsigned int s)
+    : inFile(in), outFile(out),
+    reps(r), seed(s)
+    {
 
-    	// // process aliases
-    	// input.read_aliases();
-        //
-    	// glopts.read_section(input.data.front());
-        //
-    	// // Since no output file has been specified,
-    	// // the output will go to std::cout
-    	// if (!write_aln.open(/*glopts.output_file.c_str()*/ "aln:-",
-    	// 	glopts.sim_reps - 1,
-    	// 	glopts.output_split, // false
-    	// 	glopts.output_append, // false
-    	// 	glopts.output_label)) // false
-    	// {
-    	// 	DAWG_ERROR("bad configuration");
-    	// 	return;
-    	// }
-    	// write_aln.set_blocks(glopts.output_block_head.c_str(),
-    	// 	glopts.output_block_between.c_str(),
-    	// 	glopts.output_block_tail.c_str(),
-    	// 	glopts.output_block_before.c_str(),
-    	// 	glopts.output_block_after.c_str()
-    	// );
     }
 
-    void configure() {
-        // std::vector<dawg::ma> configs;
-    	// if (!dawg::ma::from_trick(input, configs)) {
-    	// 	// DAWG_ERROR("bad configuration");
-    	// 	return;
-    	// }
-        //
-        // if (!kimura.configure(configs.begin(), configs.end())) {
-        //     DAWG_ERROR("bad configuration");
-        //     return;
-        // }
-    }
+    void run() {
+        using std::string;
+        using dawg::trick;
 
-    void align() {
+    	dawg::trick input;
+
+        bool ret = true;
+    	// for(string &ss : inFile) {
+    		ret &= trick::parse_file(input, inFile.c_str());
+    	// }
+
+    	if(!ret)
+    		std::cerr << "Failure to parse DAWG file\n";
+
+    	// process aliases
+    	input.read_aliases();
+
+    	dawg::global_options glopts;
+    	glopts.read_section(input.data.front());
+
+    	dawg::output write_aln;
+
+    	// Since no output file has been specified,
+    	// the output will go to std::cout
+    	if (!write_aln.open(/*glopts.output_file.c_str()*/ outFile.c_str(),
+    		reps - 1,
+    		false, // false
+    		false, // false
+    		false)) // false
+    	{
+    		DAWG_ERROR("bad configuration");
+    		return;
+    	}
+    	write_aln.set_blocks(glopts.output_block_head.c_str(),
+    		glopts.output_block_between.c_str(),
+    		glopts.output_block_tail.c_str(),
+    		glopts.output_block_before.c_str(),
+    		glopts.output_block_after.c_str()
+    	);
+
+    	std::vector<dawg::ma> configs;
+    	if (!dawg::ma::from_trick(input, configs)) {
+    		DAWG_ERROR("bad configuration");
+    		return;
+    	}
+
+    	// Create the object that will do all the simulation
+    	// work for us.  Configure its sections.
+    	dawg::matic kimura;
+        // if a seed was specified, use it
+    	if(seed != 0) {
+    		kimura.seed(this->seed);
+    	}
+
+    	if (!kimura.configure(configs.begin(), configs.end())) {
+    		DAWG_ERROR("bad configuration");
+    		return;
+    	}
+
     	// create sets of aligned sequences;
-    	// dawg::alignment aln;
-    	// kimura.pre_walk(aln);
-    	// for (unsigned int i = 0; i<glopts.sim_reps; ++i) {
-    	// 	kimura.walk(aln);
-    	// 	alignments.insert(alignments.end(), aln);
-    	// 	//write_aln(aln); // this would print the aln data out to std::cout or a file
-    	// }
-    }
+        std::vector<dawg::alignment> alignments;
+    	dawg::alignment aln;
+    	kimura.pre_walk(aln);
+    	for (unsigned int i = 0; i< reps; ++i) {
+    		kimura.walk(aln);
+    		alignments.insert(alignments.end(), aln);
+    		//write_aln(aln); // this would print the aln data out to std::cout or a file
+        }
+    } // run
 
-    void bark(const std::string& str) {
+    void bark(const std::string &str) {
         std::cout << str << std::endl;
     }
 
 private:
+    const std::string &inFile, &outFile;
+    const unsigned int reps, seed;
     // dawg::trick input;
     // dawg::matic kimura;
     // dawg::global_options glopts;
@@ -81,8 +112,6 @@ BOOST_PYTHON_MODULE(dawg_python)
 {
     using namespace boost::python;
     class_<DawgWalker>("DawgWalker")
-        .def("parse", &DawgWalker::parse)
-        .def("configure", &DawgWalker::configure)
-        .def("align", &DawgWalker::align)
+        .def("run", &DawgWalker::run)
         .def("bark", &DawgWalker::bark);
 }
--- boost.python/dawg.py
+++ boost.python/dawg.py
@@ -1,5 +1,5 @@
 import dawg_python
 
-akita = dawg_python.DawgWalker()
-
-print(akita.bark("rough rough rough"))
+akita = dawg_python.DawgWalker(b"../examples/basic-dna.dawg", b"fasta:-", 10, 121)
+akita.bark(b"bark bark bark")
+akita.run()
