e9257092bb1643f5c3411ea2c050b6e2339e54d7
zmertens <zmertens@asu.edu>
2017-11-17 18:08:59 -0700

    Analyze wrappers has a timer \ gitignore updated

--- analyze_wrappers.sh
+++ analyze_wrappers.sh
@@ -10,27 +10,29 @@ DEV_FASTA_FILE="dev.fasta"
 # cd ../
 # cd cython       && python dawg_cython.py > $CYTHON_FASTA_FILE
 # cd ../
-# cd swig && python dawg_swig.py > $SWIG_FASTA_FILE
+# cd swig		  && python dawg_swig.py > $SWIG_FASTA_FILE
 # cd ../
+#
+# diff $DEV_FASTA_FILE boost.python/$BOOST_FASTA_FILE
+# diff $DEV_FASTA_FILE cython/$CYTHON_FASTA_FILE
+# diff $DEV_FASTA_FILE swig/$SWIG_FASTA_FILE
 
-diff $DEV_FASTA_FILE boost.python/$BOOST_FASTA_FILE
-diff $DEV_FASTA_FILE cython/$CYTHON_FASTA_FILE
-diff $DEV_FASTA_FILE swig/$SWIG_FASTA_FILE
+# arg1=dir, arg2=filename
+function profile {
+	cd $1
+	for (( i = 0; i < 10000; i++ ))
+	do
+		python $2 > /dev/null
+	done
+	cd ../
+}
 
-# # Time Cython (approx 3m51.320s)
-# # size of PyDawg.so file is about 2gb
-# cd cython
-# for (( i = 0; i < 10000; i++ ))
-# do
-# 	python dawg_cython.py > /dev/null
-# done
-# cd ../
-
+# Time SWIG (approx 7m17.167s)
+# size of dawg.so file is about 6.6mb
 # # Time BoostPython (approx 6m45.847s)
 # # size of dawg.so file is about 702mb
-# cd boost.python
-# for (( i = 0; i < 10000; i++ ))
-# do
-# 	python dawg_boost.py > /dev/null
-# done
-# cd ../
+# # Time Cython (approx 3m51.320s)
+# # size of PyDawg.so file is about 2gb
+time profile swig/ dawg_swig.py
+time profile cython/ dawg_cython.py
+time profile boost.python/ dawg_boost.py
