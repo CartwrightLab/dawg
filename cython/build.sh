907e272032997b7a9cd789c57ec9b7e407ba0f14
zmertens <zmertens@asu.edu>
2017-11-20 13:41:03 -0700

    Add example of embedding Python interpreter for cython

--- cython/build.sh
+++ cython/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-# Should use python3
-python setup.py build_ext -if 
+python setup.py build_ext -if
+# cython --embed PyDawg.cpp -o py_dawg_main.c
+# clang -std=c11 -Wall -I/usr/include/python3.6m py_dawg_main.c -o py_dawg_main -L/usr/lib -lpython3
