f4f5f154abef5879def55747550a8ddd5374a7c9
zmertens <zmertens@asu.edu>
2018-01-23 13:05:10 -0700

    Replace boost int types with CPP std types \ Use <cstdint> header, CPP11

--- src/include/dawg/mutt.h
+++ src/include/dawg/mutt.h
@@ -2,7 +2,7 @@
 #ifndef DAWG_MUTT_H
 #define DAWG_MUTT_H
 /****************************************************************************
- *  Copyright (C) 2009 Reed A. Cartwright, PhD <reed@scit.us>               *
+ *  Copyright (C) 2009-2018 Reed A. Cartwright, PhD <reed@scit.us>          *
  ****************************************************************************/
 
 #include <dawg/details/mutt.h>
@@ -14,13 +14,14 @@
 
 #include <vector>
 
-#include <boost/config.hpp>
-#include <boost/functional/hash.hpp>
-
-#ifdef BOOST_WINDOWS
-#	include <process.h>
-#	define getpid _getpid
-#endif
+#if defined(_WIN32)
+	#include <process.h>
+	#define getpid _getpid
+#elif defined(__unix__) || defined(__linux__) || defined(__ANDROID__)
+	#include <unistd.h>
+#else
+	#error This operating system is not supported
+#endif // defined
 
 namespace dawg {
 
@@ -29,11 +30,11 @@ public:
 	typedef details::mutt_gen_default generator;
 	typedef generator::native_t uint_t;
 
-	inline void seed(boost::uint32_t s) { 
+	inline void seed(std::uint32_t s) {
 		_seed.assign(1, s);
 		gen.seed(_seed.begin(), _seed.end());
 	}
-	
+
 	template<typename _It>
 	inline void seed(_It first, _It last) {
 		_seed.assign(first, last);
@@ -42,19 +43,19 @@ public:
 		else if(_seed.size() > 1)
 			gen.seed(first, last);
 	}
-	
+
 	// returns a random double between (0,1)
 	//inline double operator()() { return rand_real(); }
 	inline double rand_real() { return gen.rand_real(); }
 	// returns random 32 or 64-bit number
 	uint_t rand_uint() { return gen.rand_native(); }
 	// returns random 64-bit number
-	boost::uint64_t rand_uint64() { return gen.rand_uint64(); }
+	std::uint64_t rand_uint64() { return gen.rand_uint64(); }
 	// returns random 32-bit number
-	boost::uint32_t rand_uint32() { return gen.rand_uint32(); }
+	std::uint32_t rand_uint32() { return gen.rand_uint32(); }
 	// returns random 32-bit number with max n-1
-	boost::uint32_t rand_uint32(boost::uint32_t n) {
-		return static_cast<boost::uint32_t>(rand_real()*n);
+	std::uint32_t rand_uint32(std::uint32_t n) {
+		return static_cast<std::uint32_t>(rand_real()*n);
 	}
 	// random boolean p prob of success
 	inline bool rand_bool(double p = 0.5) { return (rand_real() < p); }
@@ -62,50 +63,50 @@ public:
 	inline double rand_exp(double r = 1.0) { return rand_exp_zig()/r; }
 	inline double rand_exp_zig();
 	inline double rand_exp_inv();
-	
+
 	// random geometric with succcess rate p in (0,1)
 	// p(1 - p)^(k-1) for k >= 1; q = -log(1-p)
 	// mean is 1/p
-	
-	inline boost::uint32_t rand_geometric_q(double q) {
-		return 1+static_cast<boost::uint32_t>(rand_exp(q));
+
+	inline std::uint32_t rand_geometric_q(double q) {
+		return 1+static_cast<std::uint32_t>(rand_exp(q));
 	}
 
 	// random zeta with slope z
-	boost::uint32_t rand_zeta(double z);
-	
+	std::uint32_t rand_zeta(double z);
+
 	// random gamma with mean ab and var abb
 	double rand_gamma(double a, double b);
-	
+
 	// random normal with mean and sigma
 	inline double rand_normal(double m, double s) {
 		return rand_normal(s)+m;
 	}
 	// random normal with sigma
 	double rand_normal(double sigma);
-	
+
 	mutt();
-	
+
 private:
 	generator gen;
-	std::vector<boost::uint32_t> _seed;
-	
-	static const boost::uint64_t ek[256];
+	std::vector<std::uint32_t> _seed;
+
+	static const std::uint64_t ek[256];
 	static const double ew[256], ef[256];
-	
+
 	static inline double zH(double x, double z1, double z2) {
 		return pow(x+1.0, z1)*z2;
 	}
 	static inline double zHi(double x, double z1, double z2) {
 		return pow(z1*x,z2)-1.0;
-	}	
+	}
 };
 
-inline boost::uint32_t create_random_seed() {
-	boost::uint32_t v = static_cast<boost::uint32_t>(getpid());
+inline std::uint32_t create_random_seed() {
+	std::uint32_t v = static_cast<std::uint32_t>(getpid());
 	v += ((v << 15) + (v >> 3)) + 0x6ba658b3; // Spread 5-decimal PID over 32-bit number
 	v^=(v<<17); v^=(v>>13); v^=(v<<5);
-	v += static_cast<boost::uint32_t>(time(nullptr));
+	v += static_cast<std::uint32_t>(time(nullptr));
 	v^=(v<<17); v^=(v>>13); v^=(v<<5);
 	return (v == 0) ? 0x6a27d958 : (v & 0x7FFFFFFF); // return at most a 31-bit seed
 }
@@ -116,7 +117,7 @@ inline mutt::mutt() {
 
 inline double mutt::rand_exp_zig() {
 	static const double r = 7.69711747013104972;
-	boost::uint64_t a = rand_uint64();
+	std::uint64_t a = rand_uint64();
 	int b = a & 255;
 	if( a <= ek[b])
 		return a*ew[b];
@@ -130,7 +131,7 @@ inline double mutt::rand_exp_zig() {
 		a = rand_uint64();
 		b = a & 255;
 	} while(a > ek[b]);
-	
+
 	return a*ew[b];
 }
 
